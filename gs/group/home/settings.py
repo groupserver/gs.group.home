3a8a0822b805c2b91598cc6e87f2ccb5648bf4c8
Michael JasonSmith <mpj17@onlinegroups.net>
2010-10-14 16:24:16 +1100

    Removing a method that is provided by the parent class

--- gs/group/home/settings.py
+++ gs/group/home/settings.py
@@ -1,14 +1,9 @@
 # coding=utf-8
 from zope.component import createObject
-from Products.GSGroupMember.groupmembership import user_member_of_group
 from Products.GSGroupMember.groupmembership import JoinableGroupsForSite
 from simpletab import SimpleTab
 
 class SettingsTab(SimpleTab):
-    @property
-    def isMember(self):
-        userInfo = createObject('groupserver.LoggedInUser', self.context)
-        return user_member_of_group(userInfo, self.groupInfo)
 
     @property
     def isAnon(self):
