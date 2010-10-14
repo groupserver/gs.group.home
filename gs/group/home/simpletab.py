01eaf22b890807300961d40678703e337e48c411
Michael JasonSmith <mpj17@onlinegroups.net>
2010-10-14 15:54:48 +1100

    Adding support for Private groups: hiding the members, topics, and files lists.

--- gs/group/home/simpletab.py
+++ gs/group/home/simpletab.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from zope.component import createObject
+from AccessControl import getSecurityManager
 
 class SimpleTab(object):
     @property
@@ -9,4 +10,18 @@ class SimpleTab(object):
     @property
     def groupInfo(self):
         return createObject('groupserver.GroupInfo', self.context)
+
+    @property
+    def viewTopics(self):
+        # --=mpj17=-- If the user can view the messages the he or she
+        #   can view almost all of the sub-pages. This is *mostly* used
+        #   to tell the difference between a Public and Private group.
+        #   (Non-members cannot even see a Secret group, so it is not
+        #   much of an issue.)
+        #
+        # TODO: Figure out I could do this better.
+        msgs = self.context.messages
+        user = getSecurityManager().getUser()
+        retval = bool(user.has_permission('View', msgs))
+        return retval
 
