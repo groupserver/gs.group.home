61ea86da585281e7943ea22be92a7951383f7cb0
Michael JasonSmith <mpj17@onlinegroups.net>
2010-10-19 17:03:29 +1100

    Providing a more fancy way of hiding the Files and Settings tabs.

--- gs/group/home/settings.py
+++ gs/group/home/settings.py
@@ -3,8 +3,7 @@ from zope.component import createObject
 from Products.GSGroupMember.groupmembership import JoinableGroupsForSite
 from simpletab import SimpleTab
 
-class SettingsTab(SimpleTab):
-
+class UserInfoTab(SimpleTab):
     @property
     def isAnon(self):
         userInfo = createObject('groupserver.LoggedInUser', self.context)
@@ -16,4 +15,14 @@ class SettingsTab(SimpleTab):
                                         self.context)
         retval = not(self.isMember) and mailingListInfo.get_property('subscribe')
         return retval
+
+class SettingsTab(UserInfoTab):
+    @property
+    def show(self):
+        return self.isMember
+
+class FilesTab(UserInfoTab):
+    @property
+    def show(self):
+        return self.viewTopics
 
