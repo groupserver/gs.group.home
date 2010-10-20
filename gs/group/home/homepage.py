9c9967df6e881615ff58cc5fb8392d5343fd8420
Michael JasonSmith <mpj17@onlinegroups.net>
2010-10-20 12:17:14 +1100

    s/site/group/g

--- gs/group/home/homepage.py
+++ gs/group/home/homepage.py
@@ -3,8 +3,8 @@ from Products.Five import BrowserView
 from zope.component import createObject
 
 class GroupHomepage(BrowserView):
-    def __init__(self, site, request):
-        BrowserView.__init__(self, site, request)
-        self.siteInfo = createObject('groupserver.SiteInfo', site)
-        self.groupInfo = createObject('groupserver.GroupInfo', site)
+    def __init__(self, group, request):
+        BrowserView.__init__(self, group, request)
+        self.siteInfo = createObject('groupserver.SiteInfo', group)
+        self.groupInfo = createObject('groupserver.GroupInfo', group)
 
