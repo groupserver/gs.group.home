74eadc1d4a91f3cd964b3b87946e6c53a7303312
Michael JasonSmith <mpj17@onlinegroups.net>
2010-11-10 09:19:51 +1100

    Adding a canPost check and getting the email address of the group.

--- gs/group/home/topicstab.py
+++ gs/group/home/topicstab.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from zope.component import createObject
 from gs.group.base.contentprovider import GroupContentProvider
-from Products.GSGroupMember.interfaces import IGSPostingUser
+from Products.GSGroup.interfaces import IGSMailingListInfo
+from Products.GSGroupMember.usercanpost import GSGroupMemberPostingInfo
 from Products.GSGroupMember.groupmembership import user_member_of_group
 
 class TopicsTab(GroupContentProvider):
@@ -9,17 +10,26 @@ class TopicsTab(GroupContentProvider):
         GroupContentProvider.__init__(self, group, request, view)
         self.manager = manager
         self.__canPost = None
+        self.__email = None
             
     @property
     def canPost(self):
         if self.__canPost == None:
             ui = createObject('groupserver.LoggedInUser', self.context)
-            canPost = IGSPostingUser(ui)
+            canPost = GSGroupMemberPostingInfo(self.groupInfo.groupObj, ui)
             self.__canPost = canPost.canPost
         return self.__canPost
         
     @property
     def isMember(self):
+        print 'Deprecated'
         u = createObject('groupserver.LoggedInUser', self.context)
         return user_member_of_group(u, self.context)
+
+    @property
+    def email(self):
+        if self.__email == None:
+            l = IGSMailingListInfo(self.groupInfo.groupObj)
+            self.__email = l.get_property('mailto')
+        return self.__email
 
