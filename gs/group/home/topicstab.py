e484204bb9f54738e84ee597203d43ca5f934525
Michael JasonSmith <mpj17@onlinegroups.net>
2011-06-06 15:26:41 +1000

    Simplifying the Topic tab code, and following the can-post code to gs.group.member.canpost.

--- gs/group/home/topicstab.py
+++ gs/group/home/topicstab.py
@@ -1,26 +1,23 @@
 # coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from zope.component import getMultiAdapter
 from Products.GSGroup.interfaces import IGSMailingListInfo
-from Products.GSGroupMember.usercanpost import GSGroupMemberPostingInfo
+from gs.group.member.canpost.interfaces import IGSPostingUser
 from simpletab import UserInfoTab
 
 class TopicsTab(UserInfoTab):
     def __init__(self, group, request, view, manager):
         UserInfoTab.__init__(self, group, request, view, manager)
-        self.__canPost = None
-        self.__email = None
             
-    @property
+    @Lazy
     def canPost(self):
-        if self.__canPost == None:
-            canPost = GSGroupMemberPostingInfo(self.groupInfo.groupObj, 
-                                                self.userInfo)
-            self.__canPost = canPost.canPost
-        return self.__canPost
+        retval = getMultiAdapter((self.groupInfo.groupObj, self.userInfo), 
+                                  IGSPostingUser)
+        return retval
         
-    @property
+    @Lazy
     def email(self):
-        if self.__email == None:
-            l = IGSMailingListInfo(self.groupInfo.groupObj)
-            self.__email = l.get_property('mailto')
-        return self.__email
+        l = IGSMailingListInfo(self.groupInfo.groupObj)
+        retval = l.get_property('mailto')
+        return retval
 
