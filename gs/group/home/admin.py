2caf26aa94b1d9401772ce35cfb80643772f32e4
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-18 17:32:22 +1100

    Cleanup

--- gs/group/home/admin.py
+++ gs/group/home/admin.py
@@ -1,15 +1,17 @@
 # coding=utf-8
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from Products.XWFCore.XWFUtils import getOption
-from Products.GSGroupMember.groupmembership import user_division_admin_of_group, user_group_admin_of_group
+from gs.group.member.base.utils import user_division_admin_of_group, \
+    user_group_admin_of_group
 from simpletab import UserInfoTab
+
 
 class AdminTab(UserInfoTab):
     def __init__(self, group, request, view, manager):
-        UserInfoTab.__init__(self, group, request, view, manager)
-        self.__mailingListInfo = None
-        
-    @property
+        super(AdminTab, self).__init__(group, request, view, manager)
+
+    @Lazy
     def memberCount(self):
         acl_users = self.context.acl_users
         assert acl_users, 'Aquisition bites'
@@ -18,18 +20,16 @@ class AdminTab(UserInfoTab):
         retval = len(userGroup.getUsers())
         return retval
 
-    @property
+    @Lazy
     def mailingListInfo(self):
-        if self.__mailingListInfo == None:
-            self.__mailingListInfo = createObject('groupserver.MailingListInfo', 
-                                               self.context)
-        return self.__mailingListInfo
+        retval = createObject('groupserver.MailingListInfo', self.context)
+        return retval
 
     @property
     def isAnnouncement(self):
-        template = self.groupInfo.get_property('group_template')
+        template = self.groupInfo.get_property('group_template')  # FIXME
         return template == 'announcement'
-        
+
     @property
     def isSiteAdmin(self):
         return user_division_admin_of_group(self.userInfo, self.groupInfo)
@@ -41,4 +41,3 @@ class AdminTab(UserInfoTab):
     @property
     def canSetPostingLimit(self):
         return getOption(self.context, 'admin_can_set_posting_limit', True)
-
