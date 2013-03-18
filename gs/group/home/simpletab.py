68e2e107656553cc93348b9d9c1a0298c249df04
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-18 17:34:23 +1100

    Using a better import

--- gs/group/home/simpletab.py
+++ gs/group/home/simpletab.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
-from Products.GSGroupMember.groupmembership import user_member_of_group
+from gs.group.member.base import user_member_of_group
 from gs.group.base import GroupViewlet
 
 
