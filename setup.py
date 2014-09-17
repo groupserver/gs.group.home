6676e2bb2bf280effdc8860350bab973053cd215
alicemurphy <ajr@posteo.de>
2014-09-17 13:48:24 -0700

    Naming .rst files as such
    --HG--
    rename : README.txt => README.rst
    rename : docs/HISTORY.txt => docs/HISTORY.rst

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ version = get_version()
 setup(name='gs.group.home',
     version=version,
     description="The homepage for a GroupServer Group",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=open("README.rst").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.rst")).read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
